3e8d15faa63c9ea9dc8bc8b9ba632ab77e79395b
Ilgara Yusifzada <ilgarayusifzada@gmail.com>
2025-12-22 20:41:08 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
 import uuid
+
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
